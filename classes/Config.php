f4cdfbe9190b31675da7c703a26ffe755d8d98ec
Email Registration <fmjh23@gmail.com>
2017-08-22 14:55:56 +0100

    email

---
+++ classes/Config.php
@@ -0,0 +1,17 @@
+<?php
+
+class Config {
+    
+    const SMTP_HOST = 'smtp.mailtrap.io';
+    
+    const SMTP_USER = '21f3a081df603a';
+    
+    const SMTP_PASSWORD = '8336b8b1fdb87a';
+    
+    const SMTP_PORT = 2525;
+    
+}
+
+
+
+?>
