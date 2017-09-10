e8300d73ff8af7e4e6b2d4d9c2addd4fec5aeec3
jhind <fmjh23@gmail.com>
2017-09-10 10:04:46 +0100

    Change to header

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>Login and Registration App</title>
 	<link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="stylesheet" href="css/styles.css">
 	<script src="js/jquery.js"></script>
